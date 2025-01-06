deff6fb40dbfcb189598b76fd9ddc5857c0fbb6a
Naoki <gtvofromb@gmail.com>
2025-01-06 10:59:59 +0900

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import { Stint_Ultra_Condensed } from "next/font/google/index";
 import Link from "next/link";
 import styles from "./styles/index.module.scss"
 
@@ -15,9 +14,9 @@ export default function Home() {
       </section>
       
       <div className={styles.linkcontainer}>
-        <MyLink label="わたしについて" href="/" />
-        <MyLink label="サイトサンプル" href="/sample-site" />
-        <MyLink label="TypeScriptであそぶ" href="/with-ts" />
+        <MyLink label="わたしについて" href="/" waitsec={1}/>
+        <MyLink label="サイトサンプル" href="/sample-site" waitsec={1.5}/>
+        <MyLink label="TypeScriptであそぶ" href="/with-ts" waitsec={2}/>
       </div>
     </main>
 
@@ -27,15 +26,39 @@ export default function Home() {
 
 type LinkProps = {
   label:string,
-  href:string
+  href:string,
+  waitsec: number
 }
 
 function MyLink(props: LinkProps) {
+
+  const linkstyles: {[key:string]:string } = {
+    margin: '0 20px 0 20px',
+    textAlign: 'center',
+    opacity: '0%',
+    animation: `fadein 1.5s ease ${props.waitsec}s 1 alternate forwards running`
+  }
+
   return (
-    <Link href={props.href} className={styles.linkstyle}>
+    <>
+    <style>
+      {`
+      @keyframes fadein {
+        0% {
+          opacity: 0%;
+        }
+      
+        100% {
+          opacity: 100%;
+        }
+      }
+      `}
+    </style>
+    <Link href={props.href} style={linkstyles}>
       <div className={styles.linkcircle}></div>
       <label className={styles.labelstyle}>{props.label}</label>
     </Link>
+    </>
   )
 
 }
